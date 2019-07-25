6532971b79fd51f60ae6d7ecb9dea7bde9f7fe8b
Vlad Balin <gaperton@gmail.com>
2019-07-25 00:13:04 -0400

    Update index.jsx

--- examples/checklistTree/src/index.jsx
+++ examples/checklistTree/src/index.jsx
@@ -6,28 +6,29 @@ import { define, Model, Collection } from '@type-r/models'
 import { localStorageIO } from '@type-r/endpoints'
 import { useModel, useIO, pureRenderProps } from '@type-r/react'
 
-// Import checklist model definition. Think of "model" as of an observable serializable class.
+// Import checklist model definition. "Model" is a class with serializable and observable attributes.
 import { ChecklistItem } from './model'
 
 // Local counter to help us count top-level renders.
 let _renders = 0;
 
-// Type-R model for the state.
+// Type-R model for the App state.
 @define class AppState extends Model {
     // The state is persisted in localStorage
     static endpoint = localStorageIO( "/@type-r/react/examples" );
 
     static attributes = {
-        id : "checklistTree", // Persistent record needs to have an id
+        id : "checklistTree", // Persistent model needs to have an id
         items : Collection.of( ChecklistItem )
     }
 }
 
-// @define should be places before every class definition, which uses react-mvx features.
 const App = () => {
+    // Create the AppState model as a part of the local component state.
+    // If anything will change inside (no matter how deep), the component will notice it and update itself.
     const state = useModel( AppState );
 
-    // Save and restore state.
+    // Save and restore state. Executed once after mount.
     const isReady = useIO( async () => {
         window.onunload = () => state.save();
         
@@ -47,14 +48,15 @@ const App = () => {
 }
 
 const List =
-    // Render the component only if the listed props have changed.
+    // Render the component again only when the listed props changed.
     pureRenderProps({
+        // it has to know the type of each prop. The "type" is a JS constructor or Type-R attribute type annotation.
         items : Collection.of( ChecklistItem )
     },
     ({ items }) =>
         <div className='children'>
             { items.map( item => (
-                /* models have globally unique cid - client id to be used as 'key' */
+                /* models have globally unique cid - client-side id to be used as 'key' */
                 <Item key={ item.cid } model={ item } />
             ))}
         </div>
